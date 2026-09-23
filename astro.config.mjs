1070c87eb81f2ee569994e4132bb2933f777a1d2
Jon Mark Deane <jon@jonmarkdeane.com>
2026-09-23 23:23:16 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jonmarkdeane.github.io",
+  base: "/malue2/",
+  output: "static",
+
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    },
+  ],
+
+  integrations: [mdx()],
+});
